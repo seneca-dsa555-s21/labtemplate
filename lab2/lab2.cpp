8c85687655afe841e3c3990e9026abe5d8004974
Catherine Leung <>
2021-05-17 18:49:36 -0400

    starter files

---
+++ lab2/lab2.cpp
@@ -0,0 +1,22 @@
+/**************************************************/
+/*                                                */
+/*  Lab 2 Starter file                            */
+/*                                                */
+/*  Name:                                         */
+/*  Student number:                               */
+/*                                                */
+/**************************************************/
+
+/* remember to code these in an iterative only manner*/
+
+
+unsigned int factorial (unsigned int n){
+
+}
+
+double power (double base, unsigned int n){
+
+}
+unsigned int fibonacci (unsigned int n){
+	
+}
